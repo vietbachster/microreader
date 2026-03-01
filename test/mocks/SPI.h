4c7445d10e29a28c74c5c985e899b82ff821ab89
CidVonHighwind <Patricks.96@hotmail.de>
2026-03-01 15:27:28 +0100

    .

--- test/mocks/SPI.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "platform_stubs.h"
-
-// Provide a simple alias so code including <SPI.h> works with the host mock
-using SPIClass = MockSPI;
-extern MockSPI SPI;
--- test/mocks/pgmspace.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-// Minimal pgmspace mock for host builds
-#ifndef PROGMEM
-#define PROGMEM
-#endif
-#ifndef pgm_read_byte
-#define pgm_read_byte(addr) (*(const unsigned char*)(addr))
-#endif
